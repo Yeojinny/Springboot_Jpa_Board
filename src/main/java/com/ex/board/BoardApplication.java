e6d3314ff47fc073c464691189aea84ea38f86d9
Yeojinny <pdg03092@gmail.com>
2021-03-17 02:55:32 +0900

    second

--- src/main/java/com/ex/board/BoardApplication.java
+++ src/main/java/com/ex/board/BoardApplication.java
@@ -2,7 +2,9 @@ package com.ex.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 @EnableJpaAuditing //jpa auditing 활성화를 위해 추가
 @SpringBootApplication
@@ -12,5 +14,11 @@ public class BoardApplication {
 
 		SpringApplication.run(BoardApplication.class, args);
 	}
-
+		@Bean
+		public HiddenHttpMethodFilter hiddenHttpMethodFilter(){
+			HiddenHttpMethodFilter filter = new HiddenHttpMethodFilter();
+			return filter;
+		}
 }
+
+
